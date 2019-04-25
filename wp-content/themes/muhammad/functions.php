b167b57efa13071d66421bfe9a0f28f3dbf8c4f4
Muhammad <muhammad.eltaher@gmail.com>
2019-04-25 16:36:43 +0200

    new

--- wp-content/themes/muhammad/functions.php
+++ wp-content/themes/muhammad/functions.php
@@ -287,10 +287,11 @@ add_action( 'login_enqueue_scripts', 'my_login_logo' );
 */
 
 // Website speed optimization
-
+/*
 function defer_parsing_of_js ( $url ) {
   if ( FALSE === strpos( $url, '.js' ) ) return $url;
   if ( strpos( $url, 'jquery.js' ) ) return $url;
     return "$url' defer ";
 }
 add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 ); 
+*/
