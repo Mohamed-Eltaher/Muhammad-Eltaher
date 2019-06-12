1609ecd94565ec2c7cbaa1c0c6fd08eed51b9fd9
Muhammad <muhammad.eltaher@gmail.com>
2019-06-12 19:06:03 +0200

    woo

--- wp-content/themes/truewebsite/front-page.php
+++ wp-content/themes/truewebsite/front-page.php
@@ -6,7 +6,7 @@
 
 get_header(); ?>
 
-<section class="custome-header">
+	<section class="custome-header">
 	<img alt="" style=" background-image:linear-gradient( to right bottom, rgba(125, 129, 111, .8), rgba(40, 0, 131, .8)), url('<?php header_image(); ?>')" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>">
 	
 	<!--<video autoplay loop id="video-background" muted plays-inline>
@@ -20,6 +20,23 @@ get_header(); ?>
 			</div>
 		</section> 
 
+		<section class="bloge best-seller">
+			<div class="container">
+				<h1 class="blog-head special-head">Best Seller</h1>
+				<div class="blog-posts">
+					<?php if(have_posts()) { ?>
+						<div class="blog-posts">
+							<?php 
+							while(have_posts()) {
+								the_post();
+								the_content();
+							} ?>
+						</div>
+					<?php } ?>
+				</div>
+
+			</div>
+		</section>
 		<!-- Blog Section -->
 		<section class="bloge">
 			<div class="container">
@@ -37,45 +54,4 @@ get_header(); ?>
 			</div>
 		</section>
 
-		<?php 
-		// echo do_shortcode('[product_page id="486"]');
-		?>
-			
-
-		<!-- projects Section 
-
-		<?php
-
-		/*
-		$mainProjects = new WP_Query(array(
-			'post_type'  => 'project',
-			'orderby'    => 'meta_value_num',
-			'order'      =>'ASC',
-		));
-
-		if($mainProjects->have_posts()) { ?>
-			<section class="bloge projects">
-				<div class="container front-projects">
-					<h1 class="blog-head special-head">Latest Projects</h1>
-					<div class="blog-posts">
-						<?php 
-						while($mainProjects->have_posts()) {
-							$mainProjects->the_post(); ?>
-							<div class="single-post">				
-								<div class="post-content">
-									<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-									<?php the_excerpt(); ?>
-									<a href="<?php the_permalink(); ?>" class="read-more">See Project</a>
-									<div class="entry-footer">
-										<?php hamo_entry_footer(); ?>
-									</div>
-								</div>
-							</div>		
-						<?php } ?>
-					</div>
-
-				</div>
-			</section>
-		<?php } */ ?> -->
-
-		<?php get_footer(); ?>
+<?php get_footer(); ?>
