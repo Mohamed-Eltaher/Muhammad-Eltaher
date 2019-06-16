7685c156748ce8df7b191c815705db8318392496
Muhammad <muhammad.eltaher@gmail.com>
2019-06-17 00:32:14 +0200

    woo

--- wp-content/themes/truewebsite/footer.php
+++ wp-content/themes/truewebsite/footer.php
@@ -1,3 +1,16 @@
+<div class="before-footer">
+	<div class="container">
+		<div class="gurantee">
+			<ul>
+				<li> <i class="fas fa-plane fa-2x"></i>Worldwide Shipping</li>
+				<li><i class="fas fa-lock fa-2x"></i>Payment Security</li>
+				<li><i class="fas fa-headset fa-2x"></i>24/7 Customer Service</li>
+				<li><i class="fas fa-undo fa-2x"></i>14 Day Money Back Guarantee</li>
+			</ul>
+		</div>
+	</div>
+	
+</div>
 <div id="colophon" class="site-footer">
 	<div class="container">
 		<div class="col-1">
@@ -8,8 +21,11 @@
 				) );
 				?>		
 		</div>
-		<div class="col-2">
+	</div>
+	<div class="col-2">
+		<div class="container">
 			<p> 2019 Copyright &copy; Eltaher.com . All Rights Reserved.</p>
+			<img src="https://cdn.shortpixel.ai/client/q_glossy,ret_img/https://www.patch-emblem.com/wp-content/themes/patchemblem/images/payment-methods-icons.png" alt="">
 		</div>
 	</div>
 </div>
