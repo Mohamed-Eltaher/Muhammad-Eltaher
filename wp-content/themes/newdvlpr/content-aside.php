c70e9d26327d40dd2acca5b2ff63ef1ba597c1b9
Muhammad <muhammad.eltaher@gmail.com>
2019-06-24 21:06:52 +0200

    woo

--- wp-content/themes/newdvlpr/content-aside.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-	<div class="row">
-		<div class="col-sm-4">
-			hello aside post
-		</div>	
-	</div>
-</div>
--- wp-content/themes/newdvlpr/searchform.php
+++
@@ -1,5 +0,0 @@
-
-
-<form action="/" method="get">
-    <input type="text" name="s" placeholder="Search" id="search" value="<?php the_search_query(); ?>" />
-</form>
