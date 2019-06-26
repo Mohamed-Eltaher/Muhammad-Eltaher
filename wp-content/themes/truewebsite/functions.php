9918210164e06f5a66419e1d589d12192a521192
Muhammad <muhammad.eltaher@gmail.com>
2019-06-26 17:40:05 +0200

    woo

--- wp-content/themes/truewebsite/functions.php
+++ wp-content/themes/truewebsite/functions.php
@@ -370,18 +370,7 @@ function wpse_mime_types_webp( $mimes ) {
 
 // declaring woocommerce support
 function mytheme_add_woocommerce_support() {
-	add_theme_support( 'woocommerce', array(
-		'thumbnail_image_width' => 150,
-		'single_image_width'    => 300,
-        'product_grid'          => array(
-            'default_rows'    => 3,
-            'min_rows'        => 2,
-            'max_rows'        => 8,
-            'default_columns' => 4,
-            'min_columns'     => 2,
-            'max_columns'     => 5,
-        ),
-	) );
+	add_theme_support( 'woocommerce');
 }
 
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
