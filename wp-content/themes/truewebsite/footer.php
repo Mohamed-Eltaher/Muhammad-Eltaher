ef70623289cd6f971afe87dad9871d33a2e7a7ec
Muhammad <muhammad.eltaher@gmail.com>
2019-06-27 20:41:30 +0200

    woo

--- wp-content/themes/truewebsite/footer.php
+++ wp-content/themes/truewebsite/footer.php
@@ -2,7 +2,7 @@
 	<i class="fa fa-chevron-up fa-2x"></i>
 </a>
 
-<div class="before-footer">
+<div id="colophon" class="site-footer">
 	<div class="container">
 		<div class="gurantee">
 			<ul>
@@ -12,23 +12,15 @@
 				<li><i class="fas fa-undo fa-2x"></i>14 Day Money Back Guarantee</li>
 			</ul>
 		</div>
-	</div>
-	
-</div>
-<div id="colophon" class="site-footer">
-	<div class="container">
 		<div class="col-1">
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-2',
-					'menu_id'        => 'footer-menu',
-				) );
-				?>		
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-2',
+				'menu_id'        => 'footer-menu',
+			) );
+			?>		
 		</div>
-	</div>
-	<div class="col-2">
-		<div class="container">
-			
+		<div class="col-2">
 			<p> 2019 Copyright &copy; <?php bloginfo('name') ?>.com . All Rights Reserved.</p>
 			<img src="https://cdn.shortpixel.ai/client/q_glossy,ret_img/https://www.patch-emblem.com/wp-content/themes/patchemblem/images/payment-methods-icons.png" alt="">
 		</div>
