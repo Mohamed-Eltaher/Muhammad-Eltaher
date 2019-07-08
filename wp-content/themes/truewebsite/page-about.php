eaff81c4216366871e1f784cd71a1e59e7dfb354
Muhammad <muhammad.eltaher@gmail.com>
2019-07-08 18:39:57 +0200

    woo

--- wp-content/themes/truewebsite/page-about.php
+++ wp-content/themes/truewebsite/page-about.php
@@ -6,9 +6,9 @@
 			<h1 class="special-head"><?php echo get_theme_mod('head_option'); ?></h1>
 			<?php echo wpautop(get_theme_mod('text_option')); ?>
 		</div>
-		<div class="about-img">
-			<img src="<?php echo wp_get_attachment_url(get_theme_mod('me_custome_image')); ?>" alt="muhammad eltaher">
-		</div>
+		<!--<div class="about-img">
+			<img src="<?php // echo wp_get_attachment_url(get_theme_mod('me_custome_image')); ?>" alt="muhammad eltaher">
+		</div> -->
 	</div>
 </section>
 
